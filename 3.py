7728589e34332678473895cc98757af9b6309a71
killian-edits <killianedits67@gmail.com>
2026-08-30 17:57:24 +0500

    Loading Data

--- 3.py
+++ 3.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
 
-class Model(nn.module):
+class Model(nn.Module):
     def __init__(self, in_features=4, h1=8, h2=9, out_features=3):
         super().__init__()
         self.fc1 = nn.Linear(in_features, h1)
@@ -16,4 +17,13 @@ class Model(nn.module):
 
         return x
 
+torch.manual_seed(41)
+
 model = Model()
+
+url = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
+df = pd.read_csv(url)
+
+df['species'] = df['species'].map({'setosa': 0.0, 'versicolor': 1.0, 'virginica': 2.0})
+
+print(df.head())
