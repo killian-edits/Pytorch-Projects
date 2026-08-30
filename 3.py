aeec96c2ae7d48b031a36495387c23e1263b1921
killian-edits <killianedits67@gmail.com>
2026-08-30 18:41:15 +0500

    Train Neural Network

--- 3.py
+++ 3.py
@@ -8,7 +8,7 @@ class Model(nn.Module):
         super().__init__()
         self.fc1 = nn.Linear(in_features, h1)
         self.fc2 = nn.Linear(h1, h2)
-        self.fc3 = nn.Linear(h2, out_features)
+        self.out = nn.Linear(h2, out_features)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -24,6 +24,32 @@ model = Model()
 url = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
 df = pd.read_csv(url)
 
-df['species'] = df['species'].map({'setosa': 0.0, 'versicolor': 1.0, 'virginica': 2.0})
+df['species'] = df['species'].map({'setosa': 0, 'versicolor': 1, 'virginica': 2})
 
-print(df.head())
+X = df.drop('species', axis=1).values
+y = df['species'].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
+
+X_train, X_test = torch.FloatTensor(X_train), torch.FloatTensor(X_test)
+y_train, y_test = torch.LongTensor(y_train), torch.LongTensor(y_test)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+epochs = 100
+losses = []
+
+for i in range(epochs):
+    y_pred = model.forward(X_train)
+
+    loss = criterion(y_pred, y_train)
+    losses.append(loss.detach().numpy())
+
+    if i % 10 == 0:
+        print(f'Epoch {i} Loss: {loss}')
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
